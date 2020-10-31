f7881f21d19dd6bcfe80ecab71c17ae9152a9184
fakhrads <fakhriadisaputra15@gmail.com>
2020-10-31 14:05:48 +0700

    update

--- resources/views/dashboard-layout/sidenav.blade.php
+++ resources/views/dashboard-layout/sidenav.blade.php
@@ -133,6 +133,64 @@
                 </div>
               </li>
             </ul>
+            @else
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link {{ set_active('cashier.home') }}" href="{{route('cashier.home')}}">
+                  <i class="ni ni-tv-2 text-default"></i>
+                    <span class="nav-link-text">Dashboard</span>
+                  </a>
+                </li>
+            </ul>
+            <hr class="my-3">
+            <ul class="navbar-nav mb-md-3">
+              <li class="nav-item">
+                <a class="nav-link {{ set_active('cashier.products') }}" href="{{route('cashier.products')}}">
+                  <i class="ni ni-bag-17 text-default"></i>
+                    <span class="nav-link-text">Produk</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link collapsed {{ set_active('stock.add') }}" href="#navbar-tables" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-tables">
+                  <i class="ni ni-box-2 text-default"></i>
+                  <span class="nav-link-text">Stocks</span>
+                </a>
+                <div class="collapse" id="navbar-tables">
+                  <ul class="nav nav-sm flex-column">
+                    <li class="nav-item">
+                      <a href="{{route('stock.add')}}" class="nav-link "> 
+                        <span class=""> Stock In </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#" class="nav-link "> 
+                        <span class=""> Stock Out </span>
+                      </a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="#" class="nav-link">
+                        <span class=""> Stock Retur </span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link collapsed" href="#navbar-reports" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-tables">
+                  <i class="ni ni-chart-bar-32 text-default"></i>
+                  <span class="nav-link-text">Reports</span>
+                </a>
+                <div class="collapse" id="navbar-reports">
+                  <ul class="nav nav-sm flex-column">
+                    <li class="nav-item">
+                      <a href="#" class="nav-link "> 
+                        <span class=""> summary </span>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
             @endif
         </div>
       </div>
