897b340ecb473dc019745d6ef183207f75139750
fakhrads <fakhriadisaputra15@gmail.com>
2020-11-01 08:32:11 +0700

    Add product search for cashier

--- resources/views/cashier/home.blade.php
+++ resources/views/cashier/home.blade.php
@@ -3,23 +3,35 @@
 @section('content')
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard Kasir') }}</div>
+        <div class="col-md-12">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
 
+            <div class="card">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    @if(Auth::guard('admin')->check())
-                        Hello {{Auth::guard('admin')->user()->name}}
-                    @elseif(Auth::guard('cashier')->check())
-                        Hello {{Auth::guard('cashier')->user()->name}}
-                    @endif
+                    <h5 class="card-title">Pencarian produk</h5>
+                    <input type="text" class="form-control form-control-alternative" placeholder="Masukkan kode produk">
+                    <br/>
+                    <div class="text-right">
+                        <button class="btn btn-icon btn-primary" type="button">
+	                        <span class="btn-inner--icon"><i class="fas fa-search"></i></span>
+                            <span class="btn-inner--text">Cari Produk</span>
+                        </button>
+                    </div>
+                    
                 </div>
             </div>
+
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Detail Produk</h5>
+                    
+                </div>
+            </div>
+               
         </div>
     </div>
 </div>
