896a30afa994c40e4e375a53108e56d20ac8e7e6
fakhrads <fakhriadisaputra15@gmail.com>
2020-11-05 13:36:29 +0700

    Update Ri

---
+++ database/seeds/AboutUs.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AboutUs extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('about_us')->insert([
+            'name' => 'Toko Habibi',
+            'img_company' => 'assets/img/brand/favicon.png',
+            'phone' => null,
+            'address' => "Jl.",
+            'about' => "is",
+        ]);
+    }
+}
