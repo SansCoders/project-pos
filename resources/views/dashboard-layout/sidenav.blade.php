8b3d990502c24acdb9fef5ac1e4dbda06395ce65
Fachriansyah MNI <37337274+fachriansyahmni@users.noreply.github.com>
2020-11-05 13:54:06 +0700

    Update sidenav.blade.php

--- resources/views/dashboard-layout/sidenav.blade.php
+++ resources/views/dashboard-layout/sidenav.blade.php
@@ -6,7 +6,14 @@
     <div class="scrollbar-inner">
       <div class="sidenav-header d-flex  align-items-center">
         <a class="navbar-brand d-flex flex-column" href="javascript:void(0)">
-          {{-- <img src="{{ asset('assets/img/brand/blue.png') }}" class="navbar-brand-img" alt="..."> --}}
+            @php
+            $constCompany = App\AboutUs::first();
+            if($constCompany == null) {
+                $constNamaCompany = "App POS";
+            }else{
+                $constNamaCompany = $constCompany->name;
+            }
+            @endphp
           <i class="ni ni-shop"></i><h2>{{$constNamaCompany}}</h2>
         </a>
         <div class=" ml-auto ">
