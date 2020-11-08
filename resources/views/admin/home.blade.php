c78726e3e75755d81a32c27c83650284ff87021f
fachriansyahmni <fachriansyahmni@gmail.com>
2020-11-08 15:31:53 +0700

    change template in admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -15,7 +15,10 @@
                   <div class="row">
                     <div class="col">
                       <h5 class="card-title text-uppercase text-muted mb-0">Total Product</h5>
-                      <span class="h2 font-weight-bold mb-0">0</span>
+                      @php
+                          $countProduct = App\Product::count();
+                      @endphp
+                      <span class="h2 font-weight-bold mb-0">{{$countProduct}}</span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow">
@@ -33,7 +36,11 @@
                   <div class="row">
                     <div class="col">
                       <h5 class="card-title text-uppercase text-muted mb-0">Total users</h5>
-                      <span class="h2 font-weight-bold mb-0">2</span>
+                      @php
+                          $countSales = App\User::count();
+                          $countCashiers = App\Cashier::count();
+                      @endphp
+                      <span class="h2 font-weight-bold mb-0">{{$countSales+ $countCashiers}}</span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-orange text-white rounded-circle shadow">
@@ -41,10 +48,6 @@
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
@@ -54,23 +57,22 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col">
-                      <h5 class="card-title text-uppercase text-muted mb-0">Sales</h5>
-                      <span class="h2 font-weight-bold mb-0">924</span>
+                      <h5 class="card-title text-uppercase text-muted mb-0">Total Transactions</h5>
+                      @php
+                          $countFaktur = App\Faktur::count();
+                      @endphp
+                      <span class="h2 font-weight-bold mb-0">{{$countFaktur}}</span>
                     </div>
                     <div class="col-auto">
                       <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow">
-                        <i class="ni ni-money-coins"></i>
+                        <i class="ni ni-cart"></i>
                       </div>
                     </div>
                   </div>
-                  <p class="mt-3 mb-0 text-sm">
-                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                    <span class="text-nowrap">Since last month</span>
-                  </p>
                 </div>
               </div>
             </div>
-            <div class="col-xl-3 col-md-6">
+            {{-- <div class="col-xl-3 col-md-6">
               <div class="card card-stats">
                 <!-- Card body -->
                 <div class="card-body">
@@ -91,45 +93,52 @@
                   </p>
                 </div>
               </div>
-            </div>
+            </div> --}}
           </div>
         </div>
       </div>
     </div>
 @endsection
 @section('content')
-<div class="container-fluid mt--6">
+<div class="container-fluid mt--5">
     <div class="row mr-2 ml-2">
-        <div class="col-8">
+        <div class="col-lg-8 order-lg-1 order-xl-1">
             <div class="row align-items-center justify-content-start">
-                <h3 class="col-12 mb-3">This Week</h3>
-                <a href="#" class="mr-4 card-lift--hover">
-                    <div class="card shadow-sm text-center col-xs-4 ">
-                        <img class="card-img-top" src="https://2img.net/u/3613/18/20/56/avatars/512-17.gif" alt="Card image cap">
-                        <div class="card-body text-dark">
-                            { Nama Produk }
-                        </div>
-                    </div>
-                </a>
-                <a href="#" class="mr-4 card-lift--hover">
-                    <div class="card shadow-sm text-center col-xs-4 ">
-                        <img class="card-img-top" src="https://2img.net/u/3613/18/20/56/avatars/512-17.gif" alt="Card image cap">
-                        <div class="card-body text-dark">
-                            { Nama Produk }
-                        </div>
-                    </div>
-                </a>
+            <strong class="h1 text-dark">Welcome {{ Auth::user()->name }}</strong>
             </div>
             <div class="row">
-                <div class="card">
-                    <div class="card-body">
-                        asdads
+                <div class="card col-lg bg-translucent-default">
+                    <div class="card-body row">
+                        <div class="col-lg-4">
+                          <a href="{{route('admin.categorys')}}">
+                            <div class="card shadow-none text-center card-body d-flex flex-row align-items-center">
+                              <i class="fa fa-plus-circle text-success"></i>&nbsp;
+                              <strong class="text-dark">TAMBAH KATEGORI</strong>
+                            </div>
+                          </a>
+                        </div>
+                        <div class="col-lg-4">
+                          <a href="{{route('admin.users-cashier')}}">
+                            <div class="card shadow-none text-center card-body d-flex flex-row align-items-center">
+                              <i class="fa fa-plus-circle text-success"></i>&nbsp;
+                              <strong class="text-dark">TAMBAH KASIR</strong>
+                            </div>
+                          </a>
+                        </div>
+                        <div class="col-lg-4">
+                          <a href="{{route('admin.users-sales')}}">
+                            <div class="card shadow-none text-center card-body d-flex flex-row align-items-center">
+                              <i class="fa fa-plus-circle text-success"></i>&nbsp;
+                              <strong class="text-dark">TAMBAH SALES</strong>
+                            </div>
+                          </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card shadow-sm">
+        <div class="col-lg-4 order-lg-2 order-xl-2">
+            {{-- <div class="card shadow-sm">
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -139,7 +148,7 @@
 
                     {{ __('You are logged in!') }}
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 </div>
